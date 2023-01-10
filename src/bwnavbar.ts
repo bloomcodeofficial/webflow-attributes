1cbd64de25d4a86b6a6cb9d3aec5ac11e1fb5867
niklashans <104555311+niklashans@users.noreply.github.com>
2023-01-10 23:37:15 +0100

    Updated code

--- src/bwnavbar.ts
+++ src/bwnavbar.ts
@@ -1,18 +1,20 @@
-// Show the navbar if the user scrolls up
+// Adds a fixed class to the navbar element when the page has been scrolled down more than 300 pixels, and removes the fixed class when the user scrolls back up. It also adds an active class to the navbar when the user scrolls down past 400 pixels, but is scrolling up again, and removes the active class otherwise. The variable lastScrollY is being used to store the last scroll position. //
+
 let lastScrollY = window.scrollY;
-const bwNavbar = document.querySelector('[bw-navbar="component"]');
+
+const navbar = document.querySelector('[bw-navbar="component"]');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 200) {
-    bwNavbar.classList.add('is-fixed');
+  if (window.scrollY > 300) {
+    navbar.classList.add('fixed-nav');
   } else {
-    bwNavbar.classList.remove('is-fixed');
+    navbar.classList.remove('fixed-nav');
   }
 
-  if (window.scrollY < lastScrollY && window.scrollY > 200) {
-    bwNavbar.classList.add('is-active');
+  if (window.scrollY < lastScrollY && window.scrollY > 400) {
+    navbar.classList.add('active-nav');
   } else {
-    bwNavbar.classList.remove('is-active');
+    navbar.classList.remove('active-nav');
   }
 
   lastScrollY = window.scrollY;
@@ -23,13 +25,17 @@ const dropdownToggle = document.querySelector('[bw-navbar="toggle-dropdown1"]');
 const dropdown = document.querySelector('[bw-navbar="dropdown1"]');
 
 dropdownToggle.addEventListener('click', () => {
-  dropdown.classList.toggle('is-active');
+  if (window.innerWidth > 991) {
+    dropdown.classList.toggle('active-dropdown');
+    navbar?.classList.toggle('white-nav');
+  } else dropdown.classList.toggle('active-dropdown');
 
   // Disable scrolling when the link is clicked
-  if (dropdown.classList.contains('is-active')) {
+  if (dropdown.classList.contains('active-dropdown')) {
     document.body.style.overflow = 'hidden';
   } else if (window.innerWidth < 991) {
     document.body.style.overflow = 'hidden';
+    document.body.style.paddingRight = '15px';
   } else {
     document.body.style.overflow = 'auto';
   }
@@ -40,12 +46,14 @@ const mobileNavToggle = document.querySelector('[bw-navbar="mobile-toggle"]');
 const mobileNav = document.querySelector('[bw-navbar="menu"]');
 
 mobileNavToggle.addEventListener('click', () => {
-  mobileNavToggle?.classList.toggle('is-active');
-  mobileNav.classList.toggle('is-active');
+  mobileNavToggle?.classList.toggle('active-menu');
+  mobileNav.classList.toggle('active-menu');
+  navbar?.classList.toggle('white-nav');
 
   // Disable scrolling when the navbar is visible in mobile
-  if (mobileNav.classList.contains('is-active') && window.innerWidth < 991) {
+  if (mobileNav.classList.contains('active-menu') && window.innerWidth < 991) {
     document.body.style.overflow = 'hidden';
+    document.body.style.paddingRight = '15px';
   } else {
     document.body.style.overflow = 'auto';
   }
